b3f15656b15069fcd8b04e113a7aa152d967d0a7
AuxZarske <43278023+AuxZarske@users.noreply.github.com>
2019-09-06 01:07:39 -0300

    cambios en index, y especialidades

--- instituto/aplicaciones/gestionMusical/urls.py
+++ instituto/aplicaciones/gestionMusical/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,especialidades
 
 urlpatterns = [
     path('',home,name='index'),
+    path('especialidades/',especialidades,name='especialidades'),
 ]
--- instituto/aplicaciones/gestionMusical/views.py
+++ instituto/aplicaciones/gestionMusical/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def especialidades(request):
+    return render(request,'especialidades.html')
